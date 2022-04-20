ca480f42ea3b75872e45995164767f9ad1709b7f
jonathanmperes <emailmarceloperes@gmail.com>
2022-04-20 10:56:25 -0300

    Utilizando o Companion Object para implementar o contador de contas

--- src/br/com/alura/modelo/Conta.kt
+++ src/br/com/alura/modelo/Conta.kt
@@ -7,6 +7,16 @@ abstract class Conta(
     var saldo = 0.0
         protected set
 
+    companion object {
+        var total = 0
+            private set
+    }
+
+    init {
+        println("Criando conta")
+        total++
+    }
+
     fun deposita(valor: Double) {
         if (valor > 0) {
             this.saldo += valor
