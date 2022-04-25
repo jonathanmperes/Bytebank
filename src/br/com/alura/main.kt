538e1adc3cc0b58b2a5ce464a0be280e3cc47a11
jonathanmperes <emailmarceloperes@gmail.com>
2022-04-25 16:36:31 -0300

    Tratando exceptions com o try expression

--- src/br/com/alura/main.kt
+++ src/br/com/alura/main.kt
@@ -2,14 +2,40 @@ import br.com.alura.modelo.Endereco
 
 fun main() {
     println("início main")
-    10/0
+
+    val entrada: String = "1.0"
+    val valorRecebido: Double? = try {
+        entrada.toDouble()
+    } catch (e: NumberFormatException) {
+        println("Problema na conversão")
+        e.printStackTrace()
+        null
+    }
+
+    val valorComTaxa: Double? = if (valorRecebido != null) {
+        valorRecebido + 0.1
+    } else {
+        0.0
+    }
+
+    if (valorComTaxa != null) {
+        println("Valor recebido: $valorComTaxa")
+    } else {
+        println("Valor inválido")
+    }
+
     funcao1()
     println("fim main")
 }
 
 fun funcao1() {
     println("início funcao1")
-    funcao2()
+    try {
+        funcao2()
+    } catch (e: ClassCastException) {
+        e.printStackTrace()
+        println("ClassCastExceptions foi pegada")
+    }
     println("fim funcao1")
 }
 
